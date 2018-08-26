9dbbbe98274c7dc75e35d5ef346e57e242846f88
Dimitri C <couwenberg@gmail.com>
2018-08-26 17:01:15 +1000

    feat: importing json

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 install: yarn
 run server :yarn run dev-server
 run app: yarn start
+goto http://localhost:3000
--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { companyInfo: {}, employees: [] }
+  }
+
+  componentDidMount() {
+    debugger;
+    axios.get('http://localhost:8080/companyInfo').then(
+      (data) => {
+        this.setState({ companyInfo: data.companyInfo });
+      }
+    )
+    axios.get('http://localhost:8080/employees').then(
+      (data) => {
+        this.setState({ employees: data.employees });
+      }
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div></div>
     );
   }
 }
